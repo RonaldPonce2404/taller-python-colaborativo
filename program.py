41acf9b62249330160d500706ce5d59f22cdf598
John Miró <john.miiiro@gmail.com>
2026-02-04 15:18:25 -0500

    Actualizado

--- program.py
+++ program.py
@@ -3,7 +3,7 @@
 # Descripción: Mejora del programa base agregando interacción con el usuario
 
 def saludar(nombre):
-    return f"Hola {nombre}, bienvenido al taller de Git y GitHub"
+    return f"Holaaaaaa {nombre}, bienvenido al taller de Git y GitHub"
 
 usuario = input("Ingrese su nombre: ")
 print(saludar(usuario))
