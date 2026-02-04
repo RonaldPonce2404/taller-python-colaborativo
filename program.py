49052d3c1c4cb9c290a023febc78939ac1e53757
Eduardo Jose <ej861066@gmail.com>
2026-02-04 15:30:34 -0500

    Agregada función mi_funcion() con funcionalidad X

--- program.py
+++ program.py
@@ -1,7 +1,7 @@
-# programa.py
-# Programa base para el taller colaborativo
-
-def saludar():
- print("Hola, este es el programa base del taller")
-
-saludar()
+# Agregado por: [Tu Nombre]
+def mi_funcion():
+    """
+    Descripción de mi aporte
+    """
+    print("Mi contribución al proyecto colaborativo")
+    # Aquí va tu código
